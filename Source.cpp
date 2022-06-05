301b80e2ea16a9c47d002803d2ce97d1100a5ddc
Khatai Suleymanli <80199997+Khatai-Suleymanli@users.noreply.github.com>
2022-06-05 18:08:29 +0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include <iostream>
+#include<string>
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a, b, c;
+
+    // Get inputs
+    cin >> a >> b >> c;
+
+    // Print their sum to the screen
+    cout << a + b + c << "\n";
+}
